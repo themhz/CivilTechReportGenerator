b0bd0db156fc28c23d07b31bc2195cc4f2e2c47c
themhz <themhz@gmail.com>
2022-03-02 19:01:54 +0200

    finalizing first report

--- ReportGenerator/App/IReports/DevExpressDocX.cs
+++ ReportGenerator/App/IReports/DevExpressDocX.cs
@@ -53,8 +53,105 @@ namespace ReportGenerator_v1.System {
         }
                 //This function is under construction. It will be used to parse the word template document and conscrtruct the report
         //However some commans are implemented.. more to come..
-        public void parse() {                                                           
+        public void parse() {
 
+            this.reportTemplate1();
+            //this.replaceTextWithNewText("{{}}", datasource.GetValue("").ToString());
+        }
+
+        public void populatePageADetails(XmlNodeList DetailList, Table table) {
+            DocumentRange r = this.getTextRange("{{PageADetails}}");
+
+            int tableIndex = 0;
+            foreach (XmlNode node in DetailList) {
+                //List<string> rows = new List<string>();
+                Dictionary<String, String> cols = new Dictionary<string, string>();
+                foreach (XmlNode row in node) {
+                    cols.Add(row.Name, row.InnerText);
+                }                             
+                this.addTableRow(table, cols);
+                tableIndex++;                               
+            }
+         
+
+        }
+        //Τhe only way to copy and paste something is via InsertDocumentContent method
+        //https://supportcenter.devexpress.com/ticket/details/t725837/richeditdocumentserver-copy-paste-problem
+        public void copy() {
+            wordProcessor.Document.InsertDocumentContent(this.targetRange.End, this.sourceRange);
+        }
+        //Moves any table 
+        public void move() {
+            wordProcessor.Document.InsertDocumentContent(this.targetRange.End, this.sourceRange);
+            this.targetRange = this.sourceRange;
+            this.delete();
+        }
+        public void replaceTextWithNewTable(String text, int rows, int cols) {
+            this.wordProcessor.Document.BeginUpdate();
+            this.targetRange = this.getTextRange(text);
+            this.wordProcessor.Document.Tables.Create(this.targetRange.Start, rows, cols);
+            this.delete();
+        }
+        public void replaceTextWithNewText(String sourceText, String targetText) {
+            this.wordProcessor.Document.BeginUpdate();
+            this.targetRange = this.getTextRange(sourceText);
+            if(this.targetRange != null)
+                this.wordProcessor.Document.Replace(targetRange, targetText);            
+        }
+        public void replaceTextWithImage(String sourceText, String targetText) {
+            this.wordProcessor.Document.BeginUpdate();
+            this.wordProcessor.Document.Unit = DevExpress.Office.DocumentUnit.Inch;
+            this.targetRange = this.getTextRange(sourceText);
+            if (this.targetRange != null) {                
+                byte[] bytes = Convert.FromBase64String(targetText);                
+                bytes = ImageResizer.resize(bytes, 700, 700);
+                using (MemoryStream ms = new MemoryStream(bytes)) {                    
+                    DocumentImageSource image = DocumentImageSource.FromStream(ms);                    
+                    this.wordProcessor.Document.Images.Insert(this.targetRange.Start, image);
+                }
+                this.delete();
+            }
+            //this.wordProcessor.Document.Replace(targetRange, targetText);
+        }        
+        public Table getTable(int position) {
+            return this.wordProcessor.Document.Tables[position];
+        }
+        public Paragraph getParagraph(int position) {
+            return this.wordProcessor.Document.Paragraphs[position];
+        }
+        public DocumentRange getTextRange(String search) {
+            try {
+                Regex myRegEx = new Regex(search);
+                return this.wordProcessor.Document.FindAll(myRegEx).First();
+            } catch (Exception ex) {
+                return null;
+            }
+            
+        }
+        public void delete() {
+            this.wordProcessor.Document.Delete(this.targetRange);
+        }
+        private void addText(DocumentRange range, String value) {
+            this.wordProcessor.Document.InsertSingleLineText(range.Start, value);
+        }
+        private void addTableRow(Table targetTable, Dictionary<String, String> cols) {
+            int rowcount = targetTable.Rows.Count() - 1;
+            targetTable.Rows.InsertAfter(rowcount);
+            try {
+                this.wordProcessor.Document.InsertSingleLineText(targetTable[rowcount, 0].Range.Start, cols["Index"].ToString());
+                this.wordProcessor.Document.InsertSingleLineText(targetTable[rowcount, 1].Range.Start, cols["Name"].ToString());
+                this.wordProcessor.Document.InsertSingleLineText(targetTable[rowcount, 2].Range.Start, cols["Density"].ToString());
+                this.wordProcessor.Document.InsertSingleLineText(targetTable[rowcount, 3].Range.Start, cols["d"].ToString());
+                this.wordProcessor.Document.InsertSingleLineText(targetTable[rowcount, 4].Range.Start, cols["λ"].ToString());
+                this.wordProcessor.Document.InsertSingleLineText(targetTable[rowcount, 5].Range.Start, MathOperations.formatTwoDecimalWithoutRound(cols["dλ"].ToString(), 4));
+            }catch(Exception ex) {
+
+            }
+
+        }
+      
+
+        private void reportTemplate1() {
             //#Replace text with new text
             this.replaceTextWithNewText("{{Projects.ProjectName}}", datasource.GetValue("Projects.ProjectName").ToString());
             this.replaceTextWithNewText("{{Projects.Address1}}", datasource.GetValue("Projects.Address1").ToString());
@@ -103,136 +200,49 @@ namespace ReportGenerator_v1.System {
                 { "9. Υπολογισμός μέγιστου επιτρεπτού και πραγματοποιήσιμου Um του κτηρίου", "36"},
                 { "10. Υπολογισμός αθέλητου αερισμού", "38"}            };
 
-            //Report Type 1
-            XmlNodeList PageAList = ((Xml)datasource).getList("PageA");            
-            foreach(XmlNode page in PageAList) {
-                foreach (XmlNode childnode in page.ChildNodes) {
-                    var node = childnode.Name;
-                    var value = childnode.InnerText;
-                    var ID = "";
-                    
-                    if (childnode.Name == "ID") {
-                        ID = childnode.InnerText;
-                        XmlNodeList DetailList = ((Xml)datasource).getList("PageADetails[ns:PageADetailID='"+ ID + "']");
-                        this.populatePageADetails(DetailList);
-                        
-                    }
 
-                    if(childnode.Name == "Image") {
-                        this.replaceTextWithImage("{{PageA." + childnode.Name + "}}", value);
-                    } else {
-                        this.replaceTextWithNewText("{{PageA." + childnode.Name + "}}", value);
-                    }
-                }
+           
 
-                break;
+            ////Report Type 1
+            XmlNodeList PageAList = ((Xml)datasource).getList("PageA");
+            //foreach PageA
+            int counter = 0;
+            foreach (XmlNode page in PageAList) {
+                //Foreach child nodes of page A get the fields if ID, BuildingID, TypeID, RecNumber etc..                
+
+
+                //Code that creates sections and clones templates
+                DocumentRange startRange = this.getTextRange("{{START}}");
+                DocumentRange endRange = this.getTextRange("{{END}}");
+                DocumentRange totalRange = wordProcessor.Document.CreateRange(startRange.Start.ToInt(), endRange.End.ToInt());
+
+                //Copy table to a new line
+
+                wordProcessor.Document.InsertDocumentContent(wordProcessor.Document.InsertSection(endRange.End).Range.Start, totalRange);
+
+                XmlNodeList DetailList = ((Xml)datasource).getList("PageADetails[ns:PageADetailID='" + page["ID"].InnerText + "']");
+                this.populatePageADetails(DetailList, this.wordProcessor.Document.Tables[counter]);
+
+
+                this.replaceTextWithImage("{{PageA." + page["Image"].Name + "}}", page["Image"].InnerText);
+                //this.replaceTextWithNewText("{{PageA.Name}}", page["Name"].InnerText);
+                //this.replaceTextWithNewText("{{PageA.ElementTypeCase}}", page["ElementTypeCase"].InnerText);
+                //this.replaceTextWithNewText("{{PageA.Ri}}", page["Ri"].InnerText);
+                //this.replaceTextWithNewText("{{PageA.R}}", page["R"].InnerText);
+                //this.replaceTextWithNewText("{{PageA.Ra}}", page["Ra"].InnerText);
+                //this.replaceTextWithNewText("{{PageA.Rall}}", page["Rall"].InnerText);
+
+
+                
+                //this.wordProcessor.Document.Delete(startRange);
+                //this.wordProcessor.Document.Delete(endRange);
+
+
+
+                counter++;
+                if(counter==2)
+                    break;
             }
-            //this.replaceTextWithNewText("{{}}", datasource.GetValue("").ToString());
         }
-
-        public void populatePageADetails(XmlNodeList DetailList) {
-            DocumentRange r = this.getTextRange("{{PageADetails}}");
-
-            int tableIndex = 0;
-            foreach (XmlNode node in DetailList) {
-                //List<string> rows = new List<string>();
-                Dictionary<String, String> cols = new Dictionary<string, string>();
-                foreach (XmlNode row in node) {
-                    cols.Add(row.Name, row.InnerText);
-                }                             
-                this.addTableRow(this.wordProcessor.Document.Tables[0], cols);
-                tableIndex++;                               
-            }
-         
-
-        }
-        //Τhe only way to copy and paste something is via InsertDocumentContent method
-        //https://supportcenter.devexpress.com/ticket/details/t725837/richeditdocumentserver-copy-paste-problem
-        public void copy() {
-            wordProcessor.Document.InsertDocumentContent(this.targetRange.End, this.sourceRange);
-        }
-        //Moves any table 
-        public void move() {
-            wordProcessor.Document.InsertDocumentContent(this.targetRange.End, this.sourceRange);
-            this.targetRange = this.sourceRange;
-            this.delete();
-        }
-        public void replaceTextWithNewTable(String text, int rows, int cols) {
-            this.wordProcessor.Document.BeginUpdate();
-            this.targetRange = this.getTextRange(text);
-            this.wordProcessor.Document.Tables.Create(this.targetRange.Start, rows, cols);
-            this.delete();
-        }
-        public void replaceTextWithNewText(String sourceText, String targetText) {
-            this.wordProcessor.Document.BeginUpdate();
-            this.targetRange = this.getTextRange(sourceText);
-            if(this.targetRange != null)
-                this.wordProcessor.Document.Replace(targetRange, targetText);            
-        }
-        public void replaceTextWithImage(String sourceText, String targetText) {
-            this.wordProcessor.Document.BeginUpdate();
-            this.wordProcessor.Document.Unit = DevExpress.Office.DocumentUnit.Inch;
-            this.targetRange = this.getTextRange(sourceText);
-            if (this.targetRange != null) {
-
-                //data:image/gif;base64,
-                //this image is a single pixel (black)
-                byte[] bytes = Convert.FromBase64String(targetText);
-
-                ;
-                bytes = ImageResizer.resize(bytes, 700, 700);
-                using (MemoryStream ms = new MemoryStream(bytes)) {                    
-                    DocumentImageSource image = DocumentImageSource.FromStream(ms);                    
-                    this.wordProcessor.Document.Images.Insert(this.targetRange.Start, image);
-                }
-
-                this.delete();
-            }
-            //this.wordProcessor.Document.Replace(targetRange, targetText);
-        }
-        protected DocumentRange getElementRange() {
-            return null;
-        }
-        public Table getTable(int position) {
-            return this.wordProcessor.Document.Tables[position];
-        }
-        public Paragraph getParagraph(int position) {
-            return this.wordProcessor.Document.Paragraphs[position];
-        }
-        public DocumentRange getTextRange(String search) {
-            try {
-                Regex myRegEx = new Regex(search);
-                return this.wordProcessor.Document.FindAll(myRegEx).First();
-            } catch (Exception ex) {
-                return null;
-            }
-            
-        }
-        public void delete() {
-            this.wordProcessor.Document.Delete(this.targetRange);
-        }    
-        //private void addTableRows(Table targetTable, TableData tabledata) {
-        //    foreach (List<string> row in tabledata.Rows) {
-        //        addTableRow(targetTable, row);
-        //    }
-        //}
-        private void addTableRow(Table targetTable, Dictionary<String, String> cols) {
-            int rowcount = targetTable.Rows.Count() - 1;
-            targetTable.Rows.InsertAfter(rowcount);
-            
-            this.wordProcessor.Document.InsertSingleLineText(targetTable[rowcount, 0].Range.Start, cols["Index"].ToString());
-            this.wordProcessor.Document.InsertSingleLineText(targetTable[rowcount, 1].Range.Start, cols["Name"].ToString());
-            this.wordProcessor.Document.InsertSingleLineText(targetTable[rowcount, 2].Range.Start, cols["Density"].ToString());
-            this.wordProcessor.Document.InsertSingleLineText(targetTable[rowcount, 3].Range.Start, cols["d"].ToString());
-            this.wordProcessor.Document.InsertSingleLineText(targetTable[rowcount, 4].Range.Start, cols["λ"].ToString());
-            this.wordProcessor.Document.InsertSingleLineText(targetTable[rowcount, 5].Range.Start, MathOperations.formatTwoDecimalWithoutRound(cols["dλ"].ToString(), 4));
-
-
-        }
-        private void addText(DocumentRange range, String value) {
-            this.wordProcessor.Document.InsertSingleLineText(range.Start, value);
-        }
-
-
     }
 }
